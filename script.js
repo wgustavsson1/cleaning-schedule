c9c37bd368caaff6fc7e6fd2a2b20aac153a5d06
wilgu921 <wilgu921@student.liu.se>
2018-10-01 02:16:44 +0200

    fix

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ Date.prototype.getWeek = function() {
     var onejan = new Date(this.getFullYear(),0,1);
     var today = new Date(this.getFullYear(),this.getMonth(),this.getDate());
     var dayOfYear = ((today - onejan +1)/86400000);
-    return Math.ceil(dayOfYear/7)
+    return Math.ceil((dayOfYear + 1)/7)
 };
 
 var today = new Date();
